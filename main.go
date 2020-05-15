92332bd4003c689d273be5243d78056e9fc33090
xjayleex <aprilmagnificat@gmail.com>
2020-05-15 15:08:28 +0900

    method

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import(
 	"./accounts"
 	"fmt"
+	"log"
 )
 //import "github.com/xjayleex/golec/accounts"
 func main(){
@@ -10,6 +11,9 @@ func main(){
 	fmt.Println(account)
 	account.Deposit(30)
 	fmt.Println(account.Balance())
-	account.Withdraw(10)
+	err := account.Withdraw(40)
+	if err!= nil {
+		log.Fatalln(err)
+	}
 	fmt.Println(account.Balance())
 }
