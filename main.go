76756dd30166184025574cf3b34c43ce93362883
xjayleex <aprilmagnificat@gmail.com>
2020-07-30 18:38:10 +0900

    method

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "./scrapper"
+import "fmt"
+
 func main(){
-	scrapper.Scrape("scala")
+	i := 1
+	j := 99
+	fmt.Println(&i,&j)
+	i, j = j ,i
+	fmt.Println(i, j)
+	fmt.Println(&i,&j)
+	//scrapper.Scrape("scala")
 }
 
