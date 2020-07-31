8bde22ec29ffc5107c123a6267415d01af76e6df
xjayleex <aprilmagnificat@gmail.com>
2020-07-31 16:26:31 +0900

    Added custom priority queue package.

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
-
-import "fmt"
-import combi "./combination"
-
+import(
+	pqc "./priorityqueue"
+	"fmt"
+)
 func main(){
-	cb := combi.New()
-	cb.SetParams(5,3)
-	li := cb.GetLists()
-	fmt.Println(li.Len())
-	for li.Len() > 0 {
-		elem := li.Front()
-		fmt.Println(elem.Value)
-		li.Remove(elem)
+	pq := pqc.New()
+	pq.Push("jay",1)
+	pq.Push("foo",3)
+	pq.Push("bar",2)
+	pq.Push("kim",4)
+	pq.Push("park",5)
+	for !pq.IsEmpty() {
+		fmt.Println(pq.Pop().(string))
 	}
+
 }
 
---
+++ priorityqueue/pq.go
@@ -0,0 +1,94 @@
+package priorityqueue
+
+import "container/heap"
+
+type Item struct {
+	Value    interface{}
+	Priority int
+	Index    int
+}
+
+type itemHeap []*Item
+
+func (ih *itemHeap) Len() int{
+	return len(*ih)
+}
+
+func (ih *itemHeap) Less(i, j int) bool {
+	return (*ih)[i].Priority < (*ih)[j].Priority
+}
+
+func (ih *itemHeap) Front() (interface{}){
+	if len(*ih) == 0{
+		return nil
+	}
+	return (*ih)[0]
+}
+
+func (ih *itemHeap) Swap(i, j int) {
+	(*ih)[i], (*ih)[j] = (*ih)[j], (*ih)[i]
+	(*ih)[i].Index = i
+	(*ih)[j].Index = j
+}
+
+func (ih *itemHeap) Push(x interface{}){
+	item := x.(*Item)
+	item.Index = len(*ih)
+	*ih = append(*ih,item)
+}
+
+func (ih *itemHeap) Pop()(x interface{}){
+	old := *ih
+	item := old[len(old)-1]
+	*ih = old[0 : len(old)-1]
+	return item
+}
+
+
+type PriorityQueue struct{
+	itemHeap *itemHeap
+
+}
+
+func New() PriorityQueue{
+	return PriorityQueue{
+		itemHeap: &itemHeap{},
+	}
+}
+
+func (p *PriorityQueue) Len() int {
+	return p.itemHeap.Len()
+}
+
+func (p *PriorityQueue) Push(v interface{}, priority int){
+
+	newItem := &Item{
+		Value:    v,
+		Priority: priority,
+	}
+	heap.Push(p.itemHeap, newItem)
+}
+
+func (p *PriorityQueue) Pop() (interface{}){
+	if len(*p.itemHeap) == 0 {
+		return nil
+	}
+	item := heap.Pop(p.itemHeap).(*Item)
+	return item.Value
+}
+
+
+func (p *PriorityQueue) Front() (interface{}){
+	if len(*p.itemHeap) == 0 {
+		return nil
+	}
+	return p.itemHeap.Front().(*Item)
+}
+
+func (p *PriorityQueue) IsEmpty() bool{
+	if len(*p.itemHeap) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
